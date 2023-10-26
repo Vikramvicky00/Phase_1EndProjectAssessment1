7d238bc5e56a0c34f264c89f9b9149e26c106372
Vikramvicky00 <gonthinavikram811@gmail.com>
2023-10-27 00:11:07 +0530

    Add project files.

---
+++ Phase1EndProject1/Player.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Phase1EndProject1
+{
+    public class Player
+    { 
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
---
+++ Phase1EndProject1/Team.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Phase1EndProject1
+{
+    internal interface Team
+    {
+            void AddPlayer(Player player);
+            void RemovePlayer(int playerId);
+            Player GetPlayerById(int playerId);
+            List<Player> GetPlayersByName(string playerName);
+            List<Player> GetAllPlayers();
+        
+    }
+}
